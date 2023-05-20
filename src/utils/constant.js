f2eae1ca05b0f75f8cc7c3d1a94b1bdd45952ac7
jessnovindy <vidilav1100@gmail.com>
2023-05-20 08:55:55 +0700

    remove script

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
-export const SCRIPT_DATA="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
+// export const SCRIPT_DATA="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
+// <script src={SCRIPT_DATA} />
